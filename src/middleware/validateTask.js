d2c2d7b56c2ec2e626185341d1971c1bed60d1ea
Miguel Angel Gomez Yepes <migue200434@gmail.com>
2025-10-28 12:39:30 -0400

    Added search by ID and documentation

--- src/middleware/validateTask.js
+++ src/middleware/validateTask.js
@@ -1,4 +1,4 @@
-import { body } from 'express-validator';
+import { body , param } from 'express-validator';
 import { checkValidationResults } from './handleValidationErrors.js';
 
 export const validateTask = [
@@ -18,3 +18,4 @@ export const validateTask = [
 
   checkValidationResults,
 ];
+
